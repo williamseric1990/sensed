77e2170d60ee4210d371d880e4b35314d8e9bcde
Cody Maden <signedlongint@gmail.com>
2015-07-29 17:38:05 -0400

    committed setup script and updated install script to copy default config

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 apt-get install curl
 curl --silent --location http://deb.nodesource.com/setup_0.12 | sudo bash -
 apt-get install nodejs
+cp ./config/config.sample.json config.json
 npm install
 npm install forever -g
+echo 'Update config/config.json to reflect this installation.'
