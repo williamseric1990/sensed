b05f2b88d903a68c3544d5b04d338aca003b9fe3
Student <student@thinkpad-13.mesh>
2015-07-30 11:51:50 -0400

    debugging messages now configurable

--- sensed.js
+++ sensed.js
@@ -1,5 +1,11 @@
 var config = require('./config/config.json');
 
+var DEBUG = function(message) {
+    if (config.debug == true) {
+        console.log(message);
+    }
+}
+
 if (config.client) {
     require('./lib/client')();
 }
