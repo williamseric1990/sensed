dc185ba2d9f4850fa6ec154c7cb6b69f7f742f1b
Cody Maden <signedlongint@gmail.com>
2015-10-13 18:23:49 -0400

    preliminary batadv-vis module added

---
+++ modules/batadvvis.js
@@ -0,0 +1,30 @@
+var SSH = require('simple-ssh')
+var config = require('../config/config.json').batadvvis
+
+var get = function () {
+  var meshMap
+
+  var ssh = new SSH({
+    host: config.host,
+    user: config.user,
+    password: config.password
+  })
+
+  ssh.exec('batadv-vis -f json', {
+    out: function (stdout) {
+      stdout = stdout.split('\n')
+      delete stdout[stdout.length - 1]
+
+      var meshMap = []
+
+      for (line in stdout) {
+        line = stdout[line]
+        meshMap[meshMap.length] = JSON.parse(line)
+      }
+    }
+  }).start()
+
+  return meshMap
+}
+
+module.export = { get: get }
