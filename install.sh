f58d71786e38616615d811826c2612e2f883a880
Cody Maden <randallmaden1497@flpoly.org>
2016-01-27 18:49:48 -0500

    removed nodejs stuff

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get install curl
-curl -sL http://deb.nodesource.com/setup_0.12 | sudo bash -
-apt-get install nodejs
-cp ./config/config.sample.json config.json
-npm install
-npm install forever -g
-echo 'Update config/config.json to reflect this installation.'
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-forever start -l sensed.log --append -e sensed.err sensed.js
