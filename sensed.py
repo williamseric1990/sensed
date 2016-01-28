76573785c180bfb66a499c3e505666f419d51d8e
Cody Maden <randallmaden1497@flpoly.org>
2016-01-27 19:18:51 -0500

    implemented debugging output and proper config handling

--- sensed.py
+++ sensed.py
@@ -15,26 +15,26 @@ class SensedServer(socketserver.BaseRequestHandler):
         body = data[2:]
         socket = self.request[1]
         if header == '\x01\x00':
-            self._Debug(chalk.cyan, 'recieved id request')
+            self._debug(chalk.cyan, 'recieved id request')
             # collect list of sensors, etc.
             sensors = ','.join(self.server.config['sensors'].keys())
             name = self.server.config['name']
             p = name + '|' + sensors
             socket.sendto(bytes(p + '\n', 'utf-8'), self.client_address)
-            self._Debug(chalk.magenta, 'sent id data')
+            self._debug(chalk.magenta, 'sent id data')
         elif header == '\x02\x00':
             if len(body) > 0:
                 body = body.split(',')
-                self._Debug(chalk.blue,
+                self._debug(chalk.blue,
                             'recieved request for: {}'.format(repr(body)))
             else:
-                self._Debug(chalk.blue, 'recieved request for sensor data')
+                self._debug(chalk.blue, 'recieved request for sensor data')
             
             # get data for specified sensors
-            self._Debug(chalk.red, 'sensor data: ' + repr(r))
+            self._debug(chalk.red, 'sensor data: ' + repr(r))
             # r = self.to_packet(data) + '|' + int(time.time())
             # socket.sendto(bytes(r + '\n', 'utf-8'), self.client_address)
-            self._Debug(chalk.yellow, 'sent sensor data')
+            self._debug(chalk.yellow, 'sent sensor data')
 
     def to_packet(self, seq):
         ret = []
@@ -42,16 +42,20 @@ class SensedServer(socketserver.BaseRequestHandler):
             ret.append(k + ',' + seq[k])
         return ';'.join(ret) + '\n'
 
-    def _Debug(self, f, arg):
-        if self.server.verbose:
+    def _debug(self, f, arg):
+        if self.server.config['verbose']:
             f(arg)
 
+
+def _debug(verbose, f, arg):
+    if verbose:
+        f(arg)
 
 # Finds a config file in a number of default locations in a
 # Linux/Unix environment. Checks in the following places:
 #  1. /etc/sensed/
 #  2. .
-def find_config_posix(name):
+def find_config_posix():
     if os.path.isfile('/etc/sensed/config.json'):
         return '/etc/sensed/config.json'
     elif os.path.isfile('./config.json'):
@@ -63,7 +67,7 @@ def find_config_posix(name):
 # Windows environment. Checks in the following places:
 #  1. %APPDATA%\sensed\
 #  2. .
-def find_config_windows(name):
+def find_config_windows():
     if os.path.isfile(os.path.join(os.getenv('APPDATA'),
                       'sensed', 'config.json')):
         return os.path.join(os.getenv('APPDATA'), 'sensed', 'config',
@@ -81,66 +85,71 @@ def find_config():
     return find_config_posix()
 
 
-def load_config():
-    f = find_config()
+def load_config(fn=None):
+    f = fn or find_config()
     with open(f, 'r') as fp:
         config = json.loads(fp.read())
         return config
     return None
 
 
-@click.command
+@click.command()
 @click.option('--config', '-c', default=None,
               help='Configuration file for this instance.')
 @click.option('--name', '-n', default='sensed',
               help='Name of his lidard instance. Should be unique on the \
                     network. Default: sensed')
-@click.option('--sensors', '-S', default=[],
+@click.option('--sensors', '-S', default={},
               help='Sensor modules to load and enable.')
 @click.option('--port', '-p', default=3000,
               help='Port used by clients to recieve data. Default: 3000')
 @click.option('--bind', '-i', default='0.0.0.0',
               help='Binding interface for the socket server. Default: 0.0.0.0')
 @click.option('--verbose', '-V', is_flag=True,
-              help='Enable verbose output')
-def sensed():
+              help='Enable verbose output (debugging)')
+def sensed(config, name, sensors, port, bind, verbose):
     if config is None:
+        _debug(verbose, chalk.green, 'no config found, using defaults')
+        nsensors = {}
+        for s in sensors:
+            nsensors[s]['enabled'] = True
+
         cfg = {
-            'sensors': sensors,
+            'name': name,
+            'debug': verbose,
             'bind': bind,
             'port': port,
-            'meta': {
-                'name': name,
-            }
+            'sensors': nsensors
         }
     else:
-        cfg = load_config(config)
+        _debug(verbose, chalk.green, 'loading config... ')
+        cfg = load_config(fn=config)
 
         if 'sensors' not in cfg:
-            cfg['sensors'] = []
+            cfg['sensors'] = {}
         if 'bind' not in cfg:
             cfg['bind'] = '0.0.0.0'
         if 'port' not in cfg:
             cfg['port'] = port
-        if 'meta' in cfg and 'name' not in cfg['meta']:
-            cfg['meta']['name'] = 'sensed'
+        if 'name' not in cfg:
+            cfg['name'] = 'sensed'
+
+        _debug(verbose, chalk.green, 'done loading')
 
     # TODO: instantiate enabled sensor modules here
 
+    _debug(verbose, chalk.blue, 'starting server')
     server = socketserver.UDPServer((cfg['bind'], cfg['port']), SensedServer)
     server.config = cfg
-    server.verbose = verbose
 
     @atexit.register
     def close():
         chalk.blue('shutting down')
         server.server_close()
 
+    chalk.blue('sensed ready')
     server.serve_forever()
 
 
 if __name__ == '__main__':
-    config = load_config()
-    if config:
-        pass # start server
-    print('could not locate config file')
+    sensed()
