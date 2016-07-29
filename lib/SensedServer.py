b0cb9485be743239a89e51354f576486bf1a60a4
Cody Maden <randallmaden1497@flpoly.org>
2016-07-29 15:40:21 -0400

    completed implementation of sensed protocol

--- lib/SensedServer.py
+++ lib/SensedServer.py
@@ -1,24 +1,20 @@
 import time
-import socket
-import importlib
+import struct
 import msgpack
+import socketserver
 
 DATA_ID = b'\x01\x00'
 DATA_REQ = b'\x01\x01'
 DATA_ERR = b'\x01\x02'
 
-
-class SensedServer(object):
-    def setup(self):
-        if not '_initialized' in self.__dict__:
-            self.initialize()
-
+import see
+class SensedServer(socketserver.BaseRequestHandler):
     def handle(self):
-        data = self.mp_recv()
+        data, host = self.mp_recv()
         if data['header'] == DATA_ID:
             # Metadata request recieved
             packet = {'name': self.server.config['name'],
-                      'sensors': self.server.config['sensors'].keys()}
+                      'sensors': list(self.server.config['sensors'].keys())}
             header = DATA_ID
         elif data['header'] == DATA_REQ:
             # Sensor data request recieved
@@ -30,7 +26,7 @@ class SensedServer(object):
             header = DATA_ERR
 
         packet['timestamp'] = int(time.time())
-        self.send_mp(packet, header)
+        self.mp_send(header, packet, host)
 
     def get_sensors(self, sensors=[]):
         '''
@@ -39,23 +35,29 @@ class SensedServer(object):
         sensors will be queried.
         '''
         ret = {'sensors':{}}
-        for sensor in self.sensors:
-            if len(sensors) > 0 and sensor in sensors:
-                data = self.sensors[sensor].get_data()
+        for sensor in self.server.sensors:
+            if len(sensors) == 0 or sensor in sensors:
+                data = self.server.sensors[sensor].get_data()
                 ret['sensors'][sensor] = data
         return ret
 
-    def mp_send(self, data, header):
+    def mp_send(self, header, data, host):
         '''
         Sends data over the UDP socket in MessagePack format.
         First sends a four byte packet representing the size of the
         data to follow.
         '''
         mdata = header + msgpack.packb(data)
-        size = struct.pack('I', len(mdata))
-        self.sock.sendto(size, (self.host, self.port))
-        self.sock.sendto(mdata, (self.host, self.port))
+        if header == DATA_REQ:
+            size = struct.pack('I', len(mdata))
+            self.request[1].sendto(size, host)
+        self.request[1].sendto(mdata, host)
 
     def mp_recv(self):
-        data = self.sock.recv(1024)
-        return {'header': data[:2], 'body': msgpack.unpackb(data[2:])}
+        packet = self.request[0]
+        host = self.client_address
+        header = packet[:2]
+        data = packet[2:]
+        if len(data) > 0:
+            data = msgpack.unpackb(data)
+        return {'header': header, 'body': data}, host
